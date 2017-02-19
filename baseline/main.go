e638e5e834f49d42b49351ddf444012711f239f8
Alex Nichol <unixpickle@gmail.com>
2017-02-19 13:31:25 -0500

    compute average reward in baseline

--- baseline/main.go
+++ baseline/main.go
@@ -16,18 +16,22 @@ func main() {
 	var epLen int
 	var epCount int
 	flag.IntVar(&epLen, "len", 50, "episode length")
-	flag.IntVar(&epCount, "n", 100, "episode count")
+	flag.IntVar(&epCount, "n", 50000, "episode count")
 	flag.Parse()
 
 	start := cuberl.RandomStates(epCount)
 
 	var sum int
+	var rewardSum int
 	for _, x := range start {
+		starting := x.MaxSolved
 		for t := 0; t < epLen; t++ {
 			x, _ = x.Move(gocube.Move(rand.Intn(18)))
 		}
 		sum += x.MaxSolved
+		rewardSum += x.MaxSolved - starting
 	}
 
-	fmt.Println(float64(sum) / float64(epCount))
+	fmt.Println("Total solved:", float64(sum)/float64(epCount))
+	fmt.Println("Reward:", float64(rewardSum)/float64(epCount))
 }
