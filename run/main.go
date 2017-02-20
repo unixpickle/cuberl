de9b74371dc04d05a5f64a07ee1f3041ec9c4a21
Alex Nichol <unixpickle@gmail.com>
2017-02-20 09:12:48 -0500

    tool to evaluate an agent on a scramble
    I just used this tool to evaluate a network I trained overnight. It had 128 hidden units and was trained with episode length 8 and batch size 1024. It got up to an average reward of 3.8, meaning that it solved roughly 4.5 pieces on average.
    
    After running the above network on a few scrambles, it was clear that the network tries to solve pieces all around the cube rather than solving isolated chunks. It is most likely easier to solve spread-out pieces, especially when you only have 8 moves anyway.

---
+++ run/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/unixpickle/anynet/anyrnn"
+	"github.com/unixpickle/cuberl"
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/gocube"
+	"github.com/unixpickle/serializer"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var netFile string
+	var scramble string
+	var episodeLen int
+	var greedy bool
+
+	flag.StringVar(&netFile, "net", "../train/out_net", "network file")
+	flag.StringVar(&scramble, "scramble", "", "cube scramble")
+	flag.IntVar(&episodeLen, "len", 50, "episode length")
+	flag.BoolVar(&greedy, "greedy", false, "choose actions greedily")
+
+	flag.Parse()
+
+	if scramble == "" {
+		essentials.Die("Missing -scramble flag. See -help for more.")
+	}
+
+	var net anyrnn.Block
+	if err := serializer.LoadAny(netFile, &net); err != nil {
+		essentials.Die("read network:", err)
+	}
+
+	parsed, err := gocube.ParseMoves(scramble)
+	if err != nil {
+		essentials.Die("parse scramble:", err)
+	}
+
+	cube := gocube.SolvedCubieCube()
+	for _, m := range parsed {
+		cube.Move(m)
+	}
+
+	state := &cuberl.State{Cube: cube}
+	state.MaxSolved = state.NumSolved()
+	results := cuberl.AgentMoves(net, state, episodeLen, greedy)
+
+	for _, m := range results {
+		var rew float64
+		state, rew = state.Move(m)
+		fmt.Print(m.String() + " ")
+		for i := 0; i < int(rew); i++ {
+			fmt.Print("|")
+		}
+		if rew > 0 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Println()
+}
